e6ff86f2eaabb17e88dcfc942425b4450648ec75
MaxAlexander2001 <max123ah@gmail.com>
2023-05-25 09:23:53 -0500

    ETL v.1

---
+++ ETL/ETL.sql
@@ -0,0 +1,89 @@
+CREATE TABLE Dproducto (
+    DProductoID INT PRIMARY KEY AUTO_INCREMENT,
+    CodProducto VARCHAR(10) NOT NULL,
+    NombreProducto VARCHAR(100) NOT NULL,
+    PrecioCompra VARCHAR(20) NOT NULL,
+    PrecioVenta VARCHAR(20) NOT NULL,
+    CatProducto VARCHAR(100) NOT NULL,
+    FamiliaProducto VARCHAR(100) NOT NULL
+);
+
+insert into Dproducto (
+  CodProducto,
+  NombreProducto,
+  PrecioCompra,
+  PrecioVenta,
+  CatProducto,
+  FamiliaProducto
+) select p.CodProducto, p.NombreProducto, p.PrecioCompra, p.PrecioVenta, p.CatProducto, p.FamiliaProducto from Producto as p;
+
+CREATE TABLE Dcliente(
+    ClienteID INT PRIMARY KEY AUTO_INCREMENT,
+    DireccionCliente  VARCHAR(100) NULL,
+    Ciudad VARCHAR(40) NULL,
+    NombreCliente  VARCHAR(100) NULL
+);
+
+insert into Dcliente (
+    DireccionCliente,
+    Ciudad,
+    NombreCliente
+) select c.DireccionCliente, c.Ciudad, c.NombreCliente from Cliente as c;
+
+CREATE TABLE Dvendedor (
+  VendedorID INT PRIMARY KEY AUTO_INCREMENT,
+  NombreVendedor VARCHAR(100) NOT NULL
+);
+
+insert into Dvendedor (
+  NombreVendedor,
+) select v.NombreVendedor from Vendedor as v;
+
+CREATE TABLE Dtiempo (
+  TiempoID INT PRIMARY KEY AUTO_INCREMENT,
+  Fecha date DEFAULT NULL,
+  Dia varchar(10) NULL,
+  Semana varchar(100) NULL,
+  Mes varchar(20) NULL,
+  Trimestre varchar(100) NULL,
+  Anio varchar(100) NULL,
+);
+
+insert into dtiempo (
+  Fecha,
+  Dia,
+  Semana,
+  Mes,
+  Trimestre,
+  Anio
+) select t.Fecha, t.Dia, t.Semana, t.Mes, t.Trimestre, t.Anio from Tiempo as t;
+
+CREATE TABLE Hventas(
+    VentaID INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    TiempoID INT NOT NULL,
+    ProductoID INT NOT NULL,
+    ClienteID INT NOT NULL,
+    VendedorID INT NOT NULL,
+    FechaVenta  DATE NOT NULL,
+    PrecioVenta  VARCHAR(40) NULL,
+    TotalVenta  VARCHAR(40) NULL,
+    CantidadVendida VARCHAR(40) NULL,
+    PRIMARY KEY (VentaID, TiempoID, ProductoID, ClienteID, VendedorID)
+);
+
+insert into Hventas (
+    VentaID,
+    TiempoID,
+    ProductoID,
+    ClienteID,
+    VendedorID,
+    FechaVenta,
+    PrecioVenta,
+    TotalVenta,
+    CantidadVendida
+) select ven.VentaID, ven.TiempoID, ven.ProductoID, ven.ClienteID, ven.VendedorID, ven.FechaVenta, ven.PrecioVenta, ven.TotalVenta, ven.CantidadVendida from Ventas as ven;
+
+
+
+
+
--- vidriosdb.sql
+++ vidriosdb.sql
@@ -5,36 +5,36 @@ use vidriosdb;
 
 /*Table structure for table `DVendedor` */
 
-DROP TABLE IF EXISTS DVendedor;
+DROP TABLE IF EXISTS Vendedor;
 
-CREATE TABLE DVendedor (
-                DVendedorID INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE Vendedor (
+                VendedorID INT AUTO_INCREMENT NOT NULL,
                 NombreVendedor  VARCHAR(100) NOT NULL,
-                PRIMARY KEY (DVendedorID)
+                PRIMARY KEY (VendedorID)
 );
 
 /*Data for the table `DVendedor` */
 
-insert into dvendedor(NombreVendedor) values
+insert into vendedor(NombreVendedor) values
 ('Jheyson Vilca'),
 ('Max Avalos'),
 ('Rosa');
 
 /*Table structure for table `DCliente' */
 
-DROP TABLE IF EXISTS DCliente;
+DROP TABLE IF EXISTS Cliente;
 
-CREATE TABLE DCliente (
-                DClienteID INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE Cliente (
+                ClienteID INT AUTO_INCREMENT NOT NULL,
                 DireccionCliente  VARCHAR(100) NOT NULL,
                 Ciudad VARCHAR(40) NOT NULL,
                 NombreCliente  VARCHAR(100) NOT NULL,
-                PRIMARY KEY (DClienteID)
+                PRIMARY KEY (ClienteID)
 );
 
 /*Data for the table 'DCliente' */
 
-insert into dcliente (DireccionCliente, Ciudad, NombreCliente) values
+insert into cliente (DireccionCliente, Ciudad, NombreCliente) values
 ('Jr. Olmos','Juliaca','Juan Perez'),
 ('Jr. Alameda','Puno','Jorge Jimenes'),
 ('Jr. Piura','Juliaca','La torre'),
@@ -43,22 +43,22 @@ insert into dcliente (DireccionCliente, Ciudad, NombreCliente) values
 
 /*Table structure for table `DTiempo` */
 
-DROP TABLE IF EXISTS DTiempo;
+DROP TABLE IF EXISTS Tiempo;
 
-CREATE TABLE DTiempo (
-                DTiempoID INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE Tiempo (
+                TiempoID INT AUTO_INCREMENT NOT NULL,
                 Fecha DATE NOT NULL,
                 Dia VARCHAR(20) NOT NULL,
                 Semana VARCHAR(20) NOT NULL,
                 Mes VARCHAR(20) NOT NULL,
                 Trimestre VARCHAR(20) NOT NULL,
                 Anio VARCHAR(20) NOT NULL,
-                PRIMARY KEY (DTiempoID)
+                PRIMARY KEY (TiempoID)
 );
 
 /*Data for the table 'DTiempo' */
 
-insert into dtiempo (Fecha, Dia, Semana, Mes, trimestre, Anio) values
+insert into tiempo (Fecha, Dia, Semana, Mes, trimestre, Anio) values
 (now(),'Miercoles','Semana1','Mayo','Segundo','2023'),
 (now(),'Miercoles','Semana2','Mayo','Segundo','2023'),
 (now(),'Miercoles','Semana3','Mayo','Segundo','2023'),
@@ -66,22 +66,22 @@ insert into dtiempo (Fecha, Dia, Semana, Mes, trimestre, Anio) values
 
 /*Table structure for table 'DProducto' */
 
-DROP TABLE IF EXISTS DProducto;
+DROP TABLE IF EXISTS Producto;
 
-CREATE TABLE DProducto (
-                DProductoID INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE Producto (
+                ProductoID INT AUTO_INCREMENT NOT NULL,
                 CodProducto VARCHAR(10) NOT NULL,
                 NombreProducto VARCHAR(100) NOT NULL,
                 PrecioCompra VARCHAR(20) NOT NULL,
                 PrecioVenta VARCHAR(20) NOT NULL,
                 CatProducto VARCHAR(100) NOT NULL,
                 FamiliaProducto VARCHAR(100) NOT NULL,
-                PRIMARY KEY (DProductoID)
+                PRIMARY KEY (ProductoID)
 );
 
 /*Data for the table 'DProducto' */
 
-insert into dproducto (CodProducto, NombreProducto, PrecioCompra, PrecioVenta, CatProducto, FamiliaProducto) values
+insert into producto (CodProducto, NombreProducto, PrecioCompra, PrecioVenta, CatProducto, FamiliaProducto) values
 ('001','Vidrio transparente','2.00','3.00','Vidrios planos','Vidrios para construcción y decoración'),
 ('002','Vidrio templado','3.00','4.50','Vidrios planos','Vidrios para construcción y decoración'),
 ('003','Vidrio laminado','3.50','5.00','Vidrios planos','Vidrios para construcción y decoración'),
@@ -110,24 +110,24 @@ insert into dproducto (CodProducto, NombreProducto, PrecioCompra, PrecioVenta, C
 
 /*Table structure for table 'HVentas' */
 
-DROP TABLE IF EXISTS HVentas;
+DROP TABLE IF EXISTS Ventas;
 
-CREATE TABLE HVentas (
-                DVentaID INT AUTO_INCREMENT NOT NULL,
-                DTiempoID INT NOT NULL,
-                DProductoID INT NOT NULL,
-                DClienteID INT NOT NULL,
-                DVendedorID INT NOT NULL,
+CREATE TABLE Ventas (
+                VentaID INT AUTO_INCREMENT NOT NULL,
+                TiempoID INT NOT NULL,
+                ProductoID INT NOT NULL,
+                ClienteID INT NOT NULL,
+                VendedorID INT NOT NULL,
                 FechaVenta  DATE NOT NULL,
                 PrecioVenta  VARCHAR(40) NOT NULL,
                 TotalVenta  VARCHAR(40) NOT NULL,
                 CantidadVendida VARCHAR(40) NOT NULL,
-                PRIMARY KEY (DVentaID, DTiempoID, DProductoID, DClienteID, DVendedorID)
+                PRIMARY KEY (VentaID, TiempoID, ProductoID, ClienteID, VendedorID)
 );
 
 /*Data for the table 'HVentas' */
 
-insert into hventas(dtiempoid, dproductoid, dclienteid, dvendedorid, FechaVenta, PrecioVenta, TotalVenta, CantidadVendida) values
+insert into ventas(tiempoid, productoid, clienteid, vendedorid, FechaVenta, PrecioVenta, TotalVenta, CantidadVendida) values
 (1,2,2,1,now(),'18.45','22.50','5'),
 (2,1,1,2,now(),'19.68','24.00','8'),
 (3,15,1,3,now(),'24.60','30.00','4'),
@@ -135,26 +135,26 @@ insert into hventas(dtiempoid, dproductoid, dclienteid, dvendedorid, FechaVenta,
 
 -- Relación de las tablas
 
-ALTER TABLE HVentas ADD CONSTRAINT vendedor_hventas_fk
-FOREIGN KEY (DVendedorID)
-REFERENCES DVendedor (DVendedorID)
+ALTER TABLE Ventas ADD CONSTRAINT vendedor_ventas_fk
+FOREIGN KEY (VendedorID)
+REFERENCES Vendedor (VendedorID)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
-ALTER TABLE HVentas ADD CONSTRAINT cliente_hventas_fk
-FOREIGN KEY (DClienteID)
-REFERENCES DCliente (DClienteID)
+ALTER TABLE Ventas ADD CONSTRAINT cliente_ventas_fk
+FOREIGN KEY (ClienteID)
+REFERENCES Cliente (ClienteID)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
-ALTER TABLE HVentas ADD CONSTRAINT tiempo_hventas_fk
-FOREIGN KEY (DTiempoID)
-REFERENCES DTiempo (DTiempoID)
+ALTER TABLE Ventas ADD CONSTRAINT tiempo_ventas_fk
+FOREIGN KEY (TiempoID)
+REFERENCES Tiempo (TiempoID)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
 
-ALTER TABLE HVentas ADD CONSTRAINT producto_hventas_fk
-FOREIGN KEY (DProductoID)
-REFERENCES DProducto (DProductoID)
+ALTER TABLE Ventas ADD CONSTRAINT producto_ventas_fk
+FOREIGN KEY (ProductoID)
+REFERENCES Producto (ProductoID)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION;
